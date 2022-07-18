bc88229ccf16d661a005c06fe9896e9a1db02718
Kieran B <kieranbru.web@gmail.com>
2022-07-18 17:47:31 -0400

    Added rating restriction on backend

--- server/db/index.js
+++ server/db/index.js
@@ -13,6 +13,13 @@ const Movie = conn.define('movie', {
     }
 });
 
+Movie.addHook('beforeUpdate', (movie, options) => {
+    const { rating } = movie.dataValues;
+    if (rating >= 6 || rating <= 0) {
+        throw new Error('Rating cannot go below 1 or above 5.');
+    }
+});
+
 module.exports = {
     conn,
     Movie
